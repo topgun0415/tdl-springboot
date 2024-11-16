899b17860ea92e10b3f6b6a432591c43dfdf43bc
philipuuu <topgun0415@naver.com>
2024-11-16 15:37:52 +0900

    refactor : directory structure fixed & index page added

---
+++ src/main/java/AppConfig.java
@@ -0,0 +1,10 @@
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Configuration
+public class AppConfig {
+
+
+
+
+}
---
+++ src/main/java/springboot/tdl/controller/LoginController.java
@@ -0,0 +1,13 @@
+package springboot.tdl.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/")
+    public String view() {
+        return "login";
+    }
+}
